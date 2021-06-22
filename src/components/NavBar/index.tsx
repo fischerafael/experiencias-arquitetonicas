85975c9aef1ab64eb660bd6931200b5ee7957fb2
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-06-21 22:29:57 -0300

    add jit to tailwind file

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,7 +2,7 @@ export const NavBar = () => {
     return (
         <header className="h-nav flex items-center justify-between px-4">
             <img src="/icons/logo-white.svg" alt="" />
-            <a href="#" className="font-poppins text-white hover:text-gray-500">
+            <a href="#" className="font-poppins text-white hover:text-gray-50">
                 Acessar Projeto
             </a>
         </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    node: 'jit',
     purge: [
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}'
