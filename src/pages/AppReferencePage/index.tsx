5433e8fadb2c9466dda09fd6d30e690655e2c10a
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-06-23 11:16:34 -0300

    finish reference card

--- src/pages/AppReferencePage/index.tsx
+++ src/pages/AppReferencePage/index.tsx
@@ -3,7 +3,7 @@ import { AppDashboardLayout } from '../../components/Layout/AppDashboardLayout'
 import { AppDashboardlayoutList } from '../../components/Layout/AppDashboardLayoutList'
 import { BreadCrumb } from '../../components/BreadCrumb'
 
-import { BreadCrumbLinkProp } from '../../types'
+import { BreadCrumbLinkProp, IProject } from '../../types'
 
 const breadCrumbLinks: BreadCrumbLinkProp[] = [
     {
@@ -16,6 +16,32 @@ const breadCrumbLinks: BreadCrumbLinkProp[] = [
     }
 ]
 
+const rua24: IProject = {
+    thumbnail:
+        'https://www.urbs.curitiba.pr.gov.br/uploads/galeriaNoticaImagens/ac7b5bbb9f7c46de86ceedf4b9f8d142c951e964.jpg',
+    title: 'Rua 24 Horas',
+    location: 'Curitiba, 24 Horas',
+    features: [
+        'lazer',
+        'trabalho',
+        'lazer',
+        'trabalho',
+        'trabalho',
+        'lazer',
+        'trabalho',
+        'trabalho',
+        'lazer',
+        'trabalho',
+        'lazer',
+        'trabalho',
+        'trabalho',
+        'lazer',
+        'trabalho'
+    ],
+    experience: 'Muito Negativa',
+    feelings: ['irritado', 'puto', 'revoltado']
+}
+
 export const AppReferencePage = () => {
     return (
         <main className="bg-gray-50 min-h-screen flex flex-col justify-start">
@@ -23,57 +49,68 @@ export const AppReferencePage = () => {
             <AppDashboardLayout>
                 <BreadCrumb links={breadCrumbLinks} />
                 <AppDashboardlayoutList>
-                    <AppReferenceListItem />
+                    <AppReferenceListItem project={rua24} />
                 </AppDashboardlayoutList>
             </AppDashboardLayout>
         </main>
     )
 }
 
-const ruaViteEQuatroHoras =
-    'https://www.urbs.curitiba.pr.gov.br/uploads/galeriaNoticaImagens/ac7b5bbb9f7c46de86ceedf4b9f8d142c951e964.jpg'
+interface PropsAppReferenceListItem {
+    project: IProject
+}
 
-const AppReferenceListItem = () => {
+const AppReferenceListItem = ({ project }: PropsAppReferenceListItem) => {
     return (
-        <li className="border border-gray-100 bg-white text-gray-500 grid grid-cols-6 gap-4 rounded-md overflow-hidden cursor-pointer hover:shadow-lg">
+        <li className="duration-500 transition border h-40 border-gray-100 bg-white text-gray-600 grid grid-cols-7 gap-4 rounded-md overflow-hidden cursor-pointer hover:shadow-lg">
             <section className="flex items-center h-full col-span-2">
                 <img
-                    className="object-cover h-full w-full rounded-md"
-                    src={ruaViteEQuatroHoras}
+                    className="object-cover h-full w-full"
+                    src={project.thumbnail}
                     alt="Rua 24 h"
                 />
             </section>
 
-            <section className="col-span-2 py-4 flex flex-col">
+            <section className="col-span-3 py-4 flex flex-col">
                 <span className="text-tiny font-poppins font-normal">
                     REFERÊNCIA
                 </span>
-                <h1 className="text-lg font-poppins font-bold">Rua 24 Horas</h1>
+                <h1 className="text-lg font-poppins font-bold">
+                    {project.title}
+                </h1>
                 <h2 className="text-supersmall font-poppins">
-                    Curitiba, Brasil
+                    {project.location}
                 </h2>
-                <ul className="flex flex-wrap py-2 gap-1 items-center overflow-y-scroll h-20">
-                    <FeatureItem feature="lazer" />
-                    <FeatureItem feature="lazer" />
-                    <FeatureItem feature="lazer" />
-                    <FeatureItem feature="lazer" />
-                    <FeatureItem feature="lazer" />
-                    <FeatureItem feature="lazer" />
-                    <FeatureItem feature="lazer" />
-                    <FeatureItem feature="lazer" />
+                <ul className="py-2 overflow-y-scroll flex gap-2 flex-wrap h-16">
+                    {project?.features?.map((feature, index) => (
+                        <FeatureItem key={index} feature={feature} />
+                    ))}
                 </ul>
             </section>
 
-            <h1>oi</h1>
-            <h1>oi</h1>
+            <section className="col-span-2 py-4 flex flex-col border-l bg-gray-50 border-gray-100 pl-4">
+                <span className="text-tiny font-poppins font-normal">
+                    EXPERIÊNCIA
+                </span>
+                <h1 className="text-lg font-poppins font-bold">
+                    {project.experience ? project.experience : 'Não Avaliado'}
+                </h1>
+                <ul className="py-2 overflow-y-scroll flex gap-2 flex-wrap h-16 mr-4">
+                    {project.features.length
+                        ? project.feelings.map((feeling, index) => (
+                              <FeatureItem key={index} feature={feeling} />
+                          ))
+                        : null}
+                </ul>
+            </section>
         </li>
     )
 }
 
 const FeatureItem = ({ feature }: { feature: string }) => {
     return (
-        <span className="text-tiny p-1 font-poppins bg-gray-200 rounded-md">
-            {feature}
-        </span>
+        <article className="font-poppins bg-gray-200 rounded-md flex items-center h-5">
+            <span className="text-tiny p-1">{feature}</span>
+        </article>
     )
 }
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -8,3 +8,14 @@ export interface BreadCrumbLinkProp {
 export interface BreadCrumbProps {
     links: BreadCrumbLinkProp[]
 }
+
+// PROJECT
+
+export interface IProject {
+    thumbnail: string
+    title: string
+    location: string
+    features: string[]
+    experience?: string
+    feelings?: string[]
+}
