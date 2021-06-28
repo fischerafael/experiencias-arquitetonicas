81d8ca7c58b646041826f7db333db3e89a0be847
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-06-28 14:43:30 -0300

    finished new login page

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import {
     PageAppWrapper,
     PageHeaderWrapper,
@@ -5,8 +6,29 @@ import {
     PageFooterWrapper
 } from '../../../styles/components/Layout'
 import { DefaultButton } from '../../components/Button/style'
+import { CustomInput } from '../../components/Input'
+import { CustomLink } from '../../components/CustomLink'
 
 export const LoginPage = () => {
+    const [loginInfo, setLoginInfo] = useState({ identifier: '', password: '' })
+    const [disabled, setDisabled] = useState(false)
+
+    const handleUserInfoChange = (e: any, key: string) => {
+        setLoginInfo({
+            ...loginInfo,
+            [key]: e.target.value
+        })
+    }
+
+    useEffect(() => {
+        if (loginInfo.identifier === '' || loginInfo.password === '') {
+            setDisabled(true)
+            return
+        }
+
+        setDisabled(false)
+    }, [loginInfo])
+
     return (
         <PageAppWrapper>
             <PageHeaderWrapper>
@@ -16,11 +38,43 @@ export const LoginPage = () => {
             </PageHeaderWrapper>
 
             <PageMainWrapper>
-                <h1>login form</h1>
+                <section className="py-4 flex flex-row justify-center">
+                    <h2 className="font-bold text-lg">Acessar Projeto</h2>
+                </section>
+
+                <form
+                    id="login-form"
+                    className="flex flex-col gap-4 justify-center h-full"
+                    onSubmit={() => alert('oi')}
+                >
+                    <CustomInput
+                        label="Email"
+                        placeholder="ex: rafael@gmail.com"
+                        type="email"
+                        value={loginInfo.identifier}
+                        onChange={(e) => handleUserInfoChange(e, 'identifier')}
+                    />
+
+                    <CustomInput
+                        label="Senha"
+                        placeholder="ex: ********"
+                        type="password"
+                        value={loginInfo.password}
+                        onChange={(e) => handleUserInfoChange(e, 'password')}
+                    />
+                </form>
             </PageMainWrapper>
 
             <PageFooterWrapper>
-                <DefaultButton disabled={false}>Acessar Projeto</DefaultButton>
+                <DefaultButton disabled={disabled} form="login-form">
+                    Acessar Projeto
+                </DefaultButton>
+
+                <CustomLink href="/register">
+                    <p className="font-medium py-2 w-full flex flex-row justify-center hover:text-blue-500 cursor-pointer">
+                        Criar novo projeto
+                    </p>
+                </CustomLink>
             </PageFooterWrapper>
         </PageAppWrapper>
     )
