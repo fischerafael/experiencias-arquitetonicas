97b342753a7a7a2eefaf9e133b484ee2a9ebfa38
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-06-28 13:21:39 -0300

    change a few things

--- pages/app/users/edit/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { AppUserNewPage } from '../../../../src/pages/AppUserNewPage'
-
-const index = () => {
-    return <AppUserNewPage />
-}
-
-export default index
--- pages/app/users/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { AppUserPage } from '../../../src/pages/AppUserPage'
-
-const index = () => {
-    return <AppUserPage />
-}
-
-export default index
