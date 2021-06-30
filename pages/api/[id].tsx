fc2b78aad85b80fa2e76d9c1ea534c1b19de0b71
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-06-30 14:53:29 -0300

    finished ml api endpoint

--- pages/api/[id].tsx
+++ pages/api/[id].tsx
@@ -16,30 +16,78 @@ net.train(trainingData)
 const result = net.run([0, 1])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    // get projectId
+    // get projectData via id
     const { id } = req.query
 
     try {
-        const { response: resProject } = await fetch.getReferenceData(
-            id as string
+        const rawProjectData = await fetch.getReferenceData(id as string)
+        if (!rawProjectData)
+            return res.status(200).json({ architect: 'Not found' })
+
+        const {
+            height,
+            size,
+            elements,
+            shape,
+            materials,
+            texture,
+            tone,
+            primary_color,
+            secondary_color,
+            tertiary_color,
+            opennings,
+            light,
+            contrast,
+            opacity,
+            movement,
+            people,
+            context,
+            landmark,
+            context_interest,
+            time,
+            weather
+        } = rawProjectData?.response
+
+        const formatedProjectData = [
+            height,
+            size,
+            elements,
+            shape,
+            materials,
+            texture,
+            tone,
+            primary_color,
+            secondary_color,
+            tertiary_color,
+            opennings,
+            light,
+            contrast,
+            opacity,
+            movement,
+            people,
+            context,
+            landmark,
+            context_interest,
+            time,
+            weather
+        ]
+
+        const rawReferencesData = await fetch.getAllReferences(
+            rawProjectData.response.architect.id
         )
-        if (!resProject) return res.status(200).json({ architect: 'Not found' })
-
-        const { response: resReferences } = await fetch.getAllReferences(
-            '60d6057e04c0fc9490dee351'
+        const formatedTrainningData = formatTrainningData(
+            rawReferencesData?.response
         )
 
-        const projectData = resProject
-        const referencesData = resReferences
+        net.train(formatedTrainningData)
+        const result = net.run(formatedProjectData)[0]
 
-        const { predicted_evaluation } = projectData
-
-        console.log('predicted_evaluation', predicted_evaluation)
+        console.log(result)
 
         res.status(200).json({
-            project_data: projectData,
-            references_data: referencesData,
-            result: result[0]
+            predicted_evaluation: result,
+            project_data: formatedProjectData,
+            trainning_data: formatedTrainningData
         })
     } catch (e) {
         res.status(200).json({ error: 'Error fetching data' })
@@ -50,4 +98,36 @@ const getFormattedProjectData = (projectData: any) => {
     const { height } = projectData
 
     return [height]
+}
+
+const formatTrainningData = (data: any[]) => {
+    const formatedData = data.map((reference) => {
+        return {
+            input: [
+                reference.height,
+                reference.size,
+                reference.elements,
+                reference.shape,
+                reference.materials,
+                reference.texture,
+                reference.tone,
+                reference.primary_color,
+                reference.secondary_color,
+                reference.tertiary_color,
+                reference.opennings,
+                reference.light,
+                reference.contrast,
+                reference.opacity,
+                reference.movement,
+                reference.people,
+                reference.context,
+                reference.landmark,
+                reference.context_interest,
+                reference.time,
+                reference.weather
+            ],
+            output: [reference.client_evaluation]
+        }
+    })
+    return formatedData
 }
