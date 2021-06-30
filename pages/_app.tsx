ed9e97f0f142165b4e3d7c133560688d792033e4
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-06-29 23:21:09 -0300

    add nexthead element and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import '../styles/globals.css'
 
 import { AuthProvider } from '../src/hooks/useAuth'
+import Head from 'next/head'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -20,6 +21,11 @@ const theme = {
 export default function App({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <title>Ux Arch</title>
+                <meta property="og:title" content="Ux Arch" key="title" />
+                <link rel="shortcut icon" href="/icons/logo-black.svg" />
+            </Head>
             <ThemeProvider theme={theme}>
                 <AuthProvider>
                     <GlobalStyle />
