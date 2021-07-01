7f977bf871885fc2022009c5940c792ced01262e
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-07-01 17:17:51 -0300

    create hello endpoint

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,6 @@
 const url = {
     local: 'http://localhost:3000/api',
-    production: 'https://experiencias-arquitetonicas.vercel.app/api'
+    production: 'https://www.uxarch.com.br/api'
 }
 
 export const env = url.production
