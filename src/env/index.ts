a0d92f0e5e710897354614d5c6938c841a8791d2
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-07-01 17:04:47 -0300

    fix prod url

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,6 @@
 const url = {
     local: 'http://localhost:3000/api',
-    production: 'https://ux-arch-strapi.herokuapp.com/api'
+    production: 'https://experiencias-arquitetonicas.vercel.app/api'
 }
 
 export const env = url.production
